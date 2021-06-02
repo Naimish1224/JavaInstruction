b21191d3356a0f6aa8d8b8b7deb0d6d3a351c14f
naimish1224 <naimish1224@gmail.com>
2021-06-02 12:35:05 -0400

    added class

---
+++ ch02_class/src/ClassDemoApp.java
@@ -0,0 +1,43 @@
+
+public class ClassDemoApp {
+
+	public static void main(String[] args) {
+		
+		// welcome message page-39
+		
+		System.out.println("Welcome Here");
+		
+		// page 45
+		int counter = 1;
+		double unitPrice = 14.99;
+		
+		int i1 = 5;
+		int i2 = 7;
+		double d1 = 11.1;
+		double d2 = 21.444;
+		
+		
+		counter = 2;
+		unitPrice = 13.99;
+		
+		d2 = d1;
+		d1 = 7.99;
+		System.out.println(d1);
+		System.out.println(d2);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		// Goodbye message
+		
+		System.out.println("Goodbye");
+
+	}
+
+}
---
+++ ch02_class/src/ch02_class/CalculaterApp.java
@@ -0,0 +1,33 @@
+package ch02_class;
+
+public class CalculaterApp {
+
+	public static void main(String[] args) {
+	System.out.println("Welcome to the calculater app");
+	
+	
+	int n1 = 5;
+	int n2 = 7;
+	int sum = n1 + n2;
+	
+	System.out.println("sum = "+sum);
+	
+	int difference = n2 - n2;
+	System.out.println("difference = "+difference);
+	
+	
+	int product = n2 * n2;
+	System.out.println("product = "+product);
+	
+	// quotient
+	int quotient = n2 - n2;
+	System.out.println("quotient = "+(n2/n1));
+	
+	int modulas = n2%n1;
+	System.out.println("modulas = "+modulas);
+	
+	System.out.println("bye");
+
+	}
+
+}
