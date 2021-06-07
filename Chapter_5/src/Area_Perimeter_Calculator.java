7407b1ff1cf45cf621b6e5ee96f2fdef3a4c1daa
naimish1224 <naimish1224@gmail.com>
2021-06-07 19:22:55 -0400

    fixed errors

--- Chapter_5/src/Area_Perimeter_Calculator.java
+++ Chapter_5/src/Area_Perimeter_Calculator.java
@@ -1,35 +1,30 @@
 import java.util.Scanner;
+
 public class Area_Perimeter_Calculator {
 
 	public static void main(String[] args) {
-		System.out.println("Area and Perimeter Calculator");
 		Scanner sc = new Scanner(System.in);
-		
-		System.out.print("Length: ");
-		double length = sc.nextDouble();
-		System.out.print("Width: ");
-		double width = sc.nextDouble();
-		double area = ((length) * (width));
-		System.out.println("Area: " +area);
-		
-		
-		
-	
+		String choice = "Y";
+		while (choice.equalsIgnoreCase(choice)) {
+			System.out.println("Area and Perimeter Calculator");
 
-		
-//		
-//		sc.close();
-//		System.out.println("Bye");
+			double length = getNextDoubleWithinRange(sc, "Length: ", 1, 1000);
+			double width = getNextDoubleWithinRange(sc, "Width: ", 1, 1000);
+			double area = ((length) * (width));
+			System.out.println("Area: " + area);
+			double perimeter = (2 * length) + (2 * width);
+			System.out.println("Perimeter: " + perimeter);
+
+			System.out.print("Continue, Y/N? ");
+			choice = sc.next();
+		}
+		sc.close();
+		System.out.println("Thank you for using the calculator!!");
 	}
 
+	private static double getNextDoubleWithinRange(Scanner sc, String prompt, double min, double max) {
 
-private static String getNextString(Scanner sc, String prompt) {
-	System.out.print(prompt);
-	return sc.next();
-}
-
-private static double getNextdouble(Scanner sc, String prompt) {
-	System.out.print(prompt);
-	return sc.nextDouble();
-}
+		System.out.print(prompt);
+		return sc.nextDouble();
+	}
 }
