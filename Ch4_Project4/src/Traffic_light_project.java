4b275020c0bcc70d1fc706c8bccbd078d1b31a62
naimish1224 <naimish1224@gmail.com>
2021-06-07 10:06:02 -0400

    Traffic light added invalid color if

--- Ch4_Project4/src/Traffic_light_project.java
+++ Ch4_Project4/src/Traffic_light_project.java
@@ -18,17 +18,16 @@ public class Traffic_light_project {
 		System.out.print("Enter the currentcolor of the light: ");
 		String color = sc.next().toLowerCase();
 		
+		
 		if (color.equals("green")) { 
 			System.out.println("You can Go! ");
 		}
 		
 		
-		
-		
 		else if (color.equals("red")) { 
 			System.out.print("Are you turning right? Yes/No? : ");
-			String yesno = sc.next();
-			if (yesno.equals("Yes")) {
+			String yesno = sc.next().toLowerCase();
+			if (yesno.equals("yes")) {
 				System.out.println("Please turn right with caution! ");
 			}
 			else {System.out.println("Please Stop! ");
@@ -36,23 +35,28 @@ public class Traffic_light_project {
 		}
 		
 		
-		
 		else if (color.equals("yellow")) { 
 			System.out.print("How many yards away are you from Traffic Light? : ");
 			int distance = sc.nextInt();
-			if (distance > 30) {
+			if (distance >= 30) {
 				System.out.println("Please Stop slowly! ");
 			}
-			else {System.out.println("Please Go with cautious! ");
+			else {System.out.println("Please Go with caution! ");
 			}
 		}
 		
+		else {
+			System.out.println("Please enter a valid color! Try again! ");
+			System.out.println();
+		}
+		}
 		System.out.print("Continue? (y/n): ");
 		choice = sc.next();
 		
 		
 		sc.close();
-		}}
+		System.out.println("Drive Safe! ");
+		}
 		
 	
 	
@@ -62,67 +66,10 @@ public class Traffic_light_project {
 	
 	
 	
-	
-		
-		
-		
-		
-		
-		
-		
-		
-		
-	
-		
-		
-			
-			
-			
-				
-			
-		
-		
-
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 	private static boolean Bollean(java.lang.String string2) {
 		// TODO Auto-generated method stub
 		return false;
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 	private static Object Color(String string) {
 		// TODO Auto-generated method stub
