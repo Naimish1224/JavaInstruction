410b896aba2f97fc6c672271aaee82f9ebee9c09
naimish1224 <naimish1224@gmail.com>
2021-06-12 15:58:15 -0400

    -u

--- diceRollerApp/src/diceRollerApp/pidDiceRoll.java
+++ diceRollerApp/src/diceRollerApp/pidDiceRoll.java
@@ -7,10 +7,9 @@ public class pidDiceRoll {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Pig Dice Game, Good Luck!");
 		Scanner sc = new Scanner(System.in);
-		
+
 		String choice = "y";
 		while (choice.equalsIgnoreCase("y")) {
-		
 
 			int sum = 0;
 			System.out.println("Here we go!!!!");
@@ -18,13 +17,14 @@ public class pidDiceRoll {
 				int roll = (int) (Math.random() * 6 + 1);
 				sum = sum + roll;
 				if (roll == 1) {
-					System.out.println("Your dice rolled " + d +  " times");
+					System.out.println("Your dice rolled " + d + " times");
 					System.out.println("Your Score is : " + sum);
 					System.out.print("Continue, y/n?: ");
 					choice = sc.next();
-				}
+					}
+
 			}
-		}	
+		}
 	}
 
 }
