9438a1a70eb670f42c67acc61be551a357e61115
naimish1224 <naimish1224@gmail.com>
2021-06-21 14:34:11 -0400

    -u

---
+++ ItemManagerApp/src/db/itemDB.java
@@ -0,0 +1,46 @@
+package db;
+
+import java.util.List;
+
+import business.Item;
+import interfaces.DAO;
+
+public class itemDB implements DAO<Item> {
+
+	@Override
+	public Item get(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Item> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean add(Item t) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean update(Item t) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(Item t) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean uptade(Item item) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
